4a09ddba7d90003f2e221333425b20a4d868d250
luciussulla <maciek.kowalczyk@yandex.com>
2021-06-08 20:20:41 +0200

    adding new user from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,65 @@
 import './App.css';
 import React from 'react';
 import '../index.css';
-import Word from './Word'
+
+const API = 'https://randomuser.me/api/?results=1';
+
+const ButtonFetchUsers = ({click})=> (
+  <button onClick={click}>pobierz usersow</button>
+)
+  
+const UsersList = ({users})=> {
+  const usersDiv = users.map(user=> (
+  <div key={user.id.value}>
+    <img src={user.picture.large} alt={user.name.last}/>
+    <h4>{user.name.title} {user.name.last}</h4>
+    <p>{user.email}</p>
+  </div>
+  ))
+  
+  return(
+    <div>
+      { 
+        usersDiv
+      }
+    </div> 
+  ) 
+}
 
 class App extends React.Component {
 
   state = {
-      isLoaded: false,
-      words: []
+    users: []
   }
 
-  fetchData = ()=> {
-    fetch('data/words.json')
+  handleDataFetch = ()=> {
+    console.log("click")
+    fetch(API)
+    .then(res=> {
+      if(res.ok === true) {
+        return res
+      } 
+      throw new Error(res.status) 
+    })
     .then(res => res.json())
     .then(data => {
+      const user = data.results[0]
+      const users = this.state.users
+      const newUsers = [...users, user]
       this.setState({
-        words: data.words, 
-        isLoaded: true,
+        users: newUsers
       })
     })
+    .catch(error => console.log(error))
   }
-
-  componentDidMount = ()=> {
-    setTimeout(this.fetchData, 1000)
-  }
-
+  
   render () {    
-    const words = this.state.words.map(word=>(
-      <Word key={word.id} english={word.en} polish={word.pl}/>
-    ))
+    const users = this.state.users
     return (
       <> 
-        <ul>
-          {this.state.isLoaded ? words : <p>This is only a spinner</p>}
-        </ul>
-      </>
+          <ButtonFetchUsers click={this.handleDataFetch}/>
+          {users ?  <UsersList users={users}/> : null}
+      </> 
     )
   }
 }
